1c840ca5d77cbc9310b7ae0e1df08e74437cc2b9
Eric Ta <ericta90@gmail.com>
2023-11-06 14:31:08 +0700

    update

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 import modelData from './body.json' assert {type: 'json'}
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xffffff );
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
@@ -21,14 +23,14 @@ window.updateMorphTargets = (values) => {
   bodyModel.morphTargetInfluences = values;
 };
 
-window.loadModel = () => {
+window.loadModel = (initialMorphTargets) => {
   const objData = JSON.stringify(modelData);
 
   camera.position.z = 10;
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
   scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 3.0);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2.0);
   scene.add(directionalLight);
 
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -41,18 +43,28 @@ window.loadModel = () => {
   controls.addEventListener("change", render);
 
   window.addEventListener("resize", onWindowResize);
+  //dracoLoader loader
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath('https://unpkg.com/three@0.154.x/examples/jsm/libs/draco/gltf/'); // use a full url path
 
   const loader = new GLTFLoader();
+  loader.setDRACOLoader(dracoLoader);
+
   const onLoad = (result) => {
     result.scene.traverse((child) => {
       if (child.material) child.material.metalness = 0;
     });
     const model = result.scene.children[0];
     bodyModel = model;
+    bodyModel.rotateZ(-(Math.PI / 2.5));
     console.log(model);
     console.log(model.morphTargetDictionary);
     model.scale.set(3, 3, 3);
     model.translateZ(2.2);
+    if (initialMorphTargets) {
+      bodyModel.morphTargetInfluences = initialMorphTargets;
+    }
+    
     scene.add(model);
     animate();
   };
